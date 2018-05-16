d4bf31a599f87b43c9b74ad1a37e2ed379b82c55
Kelly Stoops <kstoops815@gmail.com>
2018-05-16 08:13:22 -0500

    palindromes working

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -10,8 +10,44 @@ namespace Palindromes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please input a word or phrase:");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"You entered {input}");
+
+            string charString = input.Replace(" ", string.Empty).ToLower().ToString();
+
+            var newCharString = "";
+
+            foreach (char c in charString)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    newCharString += c;
+                }
+            }
+
+
+            string reverseCharString = new string(newCharString.Reverse().ToArray());
+
+            Console.WriteLine($"The word or phrase '{input}' written backwards is {reverseCharString}.");
+
+            if(newCharString == reverseCharString)
+            {
+                Console.WriteLine($"The word or phrase '{input}' IS a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine($"The word or phrase '{input}' IS NOT a palindrome. ");
+            }
+
+
+
+
             Console.ReadKey();
         }
     }
+    
 }
+        
